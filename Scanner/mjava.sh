58e41f798f79922988c35d0d1cea21b7317c1596
baocq <baocq@fenbi.com>
2022-06-16 23:00:40 +0800

    fix: test

--- Scanner/mjava.sh
+++ Scanner/mjava.sh
@@ -2,11 +2,17 @@
 
 echo "===== step 1 ====="
 java -classpath ../third-part/JFlex/jflex-1.8.2/lib/jflex-full-1.8.2.jar jflex.Main *.flex
-if [ $? = 0 ]; then
-  echo "===== step 2 =====" 
-  javac -Xlint:deprecation -classpath ../third-part/CUP/java-cup-11a.jar Scanner.java sym.java
-  if [ $? = 0 ]; then
-    echo "===== step 3 ====="
-    java -classpath ../third-part/CUP/java-cup-11a-runtime.jar Scanner SampleProgram.mjava
-  fi
+
+if [ $? != 0 ]; then
+  exit 1
 fi
+
+echo "===== step 2 =====" 
+javac -Xlint:deprecation -classpath ../third-part/CUP/java-cup-11a.jar Scanner.java sym.java
+
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+echo "===== step 3 ====="
+java -classpath ../third-part/CUP/java-cup-11a-runtime.jar Scanner SampleProgram.mjava
