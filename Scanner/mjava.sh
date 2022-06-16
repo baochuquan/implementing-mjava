71e5cc6c2ba107290ccc2cb2632adeb9907b6875
baocq <baocq@fenbi.com>
2022-06-16 21:10:00 +0800

    chore: scanner

--- Scanner/mjava.sh
+++ Scanner/mjava.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-java -classpath ../third-part/JFlex/jflex-1.8.2/lib/jflex-full-1.8.2.jar JFlex.Main *.flex
-if [ $? = 0 ];then
-  javac -classpath ./third-part/java-cup-11a.jar Scanner.java sym.java
+echo "===== step 1 ====="
+java -classpath ../third-part/JFlex/jflex-1.8.2/lib/jflex-full-1.8.2.jar jflex.Main *.flex
+if [ $? = 0 ]; then
+  echo "===== step 2 =====" 
+  javac -Xlint:deprecation -classpath ../third-part/CUP/java-cup-11a.jar Scanner.java sym.java
   if [ $? = 0 ]; then
-    java -classpath ./third-part/CUP/java-cup-11a-runtime.jar Scanner SampleProgram.mjava
+    echo "===== step 3 ====="
+    java -classpath ../third-part/CUP/java-cup-11a-runtime.jar Scanner SampleProgram.mjava
   fi
 fi
